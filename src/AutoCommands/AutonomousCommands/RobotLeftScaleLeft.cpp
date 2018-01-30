2b5268625c3dd055a1a20fe6cc488d077b4361f5
C <fortraan1221@gmail.com>
2018-01-29 21:50:20 -0600

    merge

--- src/AutoCommands/AutonomousCommands/RobotLeftScaleLeft.cpp
+++ src/AutoCommands/AutonomousCommands/RobotLeftScaleLeft.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "RobotLeftScaleLeft.h"
 #include <Commands/Drive/DriveToPoint.h>
 #include <Commands/Drive/RotateToPoint.h>
@@ -29,35 +28,3 @@ RobotLeftScaleLeft::RobotLeftScaleLeft() {
 		AddSequential(new IntakeAngleAdjust());
 		AddSequential(new LaunchPowerCube());
 }
-=======
-#include "RobotLeftScaleLeft.h"
-#include <Commands/Drive/DriveToPoint.h>
-#include <Commands/Drive/RotateToPoint.h>
-#include <Commands/Lift/LiftUp.h>
-#include <Commands/Intake/IntakeAngleAdjust.h>
-#include <Commands/Launch/LaunchPowerCube.h>
-
-RobotLeftScaleLeft::RobotLeftScaleLeft() {
-	// Add Commands here:
-	// e.g. AddSequential(new Command1());
-	//      AddSequential(new Command2());
-	// these will run in order.
-
-	// To run multiple commands at the same time,
-	// use AddParallel()
-	// e.g. AddParallel(new Command1());
-	//      AddSequential(new Command2());
-	// Command1 and Command2 will run in parallel.
-
-	// A command group will require all of the subsystems that each member
-	// would require.
-	// e.g. if Command1 requires chassis, and Command2 requires arm,
-	// a CommandGroup containing them would require both the chassis and the
-	// arm.
-		AddSequential(new DriveToPoint());
-		AddSequential(new RotateToPoint());
-		AddParallel(new LiftUp());
-		AddSequential(new IntakeAngleAdjust());
-		AddSequential(new LaunchPowerCube());
-}
->>>>>>> 7c4ef7586961181d848fb171a63752b73ecdac32
